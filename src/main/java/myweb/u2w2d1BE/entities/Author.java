110619e8f4ba4324e314aa1e43587c6fa14330ae
IsuruFerna <isurumadushan.ferna@gmail.com>
2024-01-17 16:40:52 +0100

    relationship craeted

--- src/main/java/myweb/u2w2d1BE/entities/Author.java
+++ src/main/java/myweb/u2w2d1BE/entities/Author.java
@@ -1,22 +1,32 @@
 package myweb.u2w2d1BE.entities;
 
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Random;
 
+@Entity
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
 public class Author {
+    @Id
+    @GeneratedValue
     private long id;
     private String firstName;
     private String lastName;
     private String email;
     private LocalDate birthDay;
     private String avatar;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL)
+    List<BlogPost> blogPostList;
 
     public Author(String firstName, String lastName, String email, String birthDay, String avatar) {
         this.firstName = firstName;
--- src/main/java/myweb/u2w2d1BE/entities/BlogPost.java
+++ src/main/java/myweb/u2w2d1BE/entities/BlogPost.java
@@ -1,23 +1,30 @@
 package myweb.u2w2d1BE.entities;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.util.Random;
 import java.util.Stack;
 
+@Entity
 @ToString
 @Getter
 @Setter
+@NoArgsConstructor
 public class BlogPost {
+    @Id
+    @GeneratedValue
+//    @Setter(AccessLevel.NONE)
     private long id;
     private String category;
     private String title;
     private String cover;
     private String content;
     private int readTime;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Author author;
 
     public BlogPost(String category, String title, String content, int readTime) {
         this.category = category;
